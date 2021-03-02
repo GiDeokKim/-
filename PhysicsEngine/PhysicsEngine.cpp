11a0013abb1dcf27118c7af3f415710b95b08e7b
gideok <ikid4127@naver.com>
2021-03-02 23:21:50 +0900

    주석 작업 테스트

--- PhysicsEngine/PhysicsEngine.cpp
+++ PhysicsEngine/PhysicsEngine.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// 물리 엔진을 만들기 위한 main 코드입니다.
+// 주석은 앞으로 Doxygen 문법에 맞춰서 수정
+
 int main()
 {
-	int a = 1;
+	int a = 1; //!< 정수 1
 	cout << a << endl;
 	return 0;
 }
